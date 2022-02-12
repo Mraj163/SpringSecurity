ba83995928f37b2490308c01f7818cca26edbc1a
Mraj <mayankarajdbg163@gmail.com>
2022-02-12 12:05:42 +0530

    Adding All Code Upto Section 2

---
+++ springsecuritybasicwithsecurity/src/main/java/com/kewal/springsecuritybasicwithsecurity/SpringsecuritybasicwithsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kewal.springsecuritybasicwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicwithsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicwithsecurityApplication.class, args);
+	}
+
+}
---
+++ springsecuritybasicwithsecurity/src/test/java/com/kewal/springsecuritybasicwithsecurity/SpringsecuritybasicwithsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kewal.springsecuritybasicwithsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicwithsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
